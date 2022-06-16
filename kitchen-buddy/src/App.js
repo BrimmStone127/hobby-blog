300142b0eae021c32c4cf2b88ea036cd7cdaa0f7
claybrimm <claybrimm@gmail.com>
2022-06-16 15:54:40 -0400

    Added photo gallery

--- kitchen-buddy/src/App.js
+++ kitchen-buddy/src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Counter} from './features/counter/Counter'
+import { PhotoGallery } from './PhotoGallery';
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <h1>Kitchen Buddy</h1>
-        <Button>Test Button</Button>
-        <Counter/>
+        {/*
+          <h1>Kitchen Buddy</h1>
+          <Button>Test Button</Button>
+          <Counter/>
+        */}
+        <PhotoGallery />
       </Container>
     </div>
   );
---
+++ kitchen-buddy/src/photos.js
@@ -0,0 +1,257 @@
+export const photos = [
+    {
+        src: "https://clayimages.s3.amazonaws.com/img003.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img029.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img058.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img059.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img061 (2).jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img070.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img097.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img100.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img101.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img103.jpg",
+        width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img104.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img105.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img109.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img111.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img112.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img113.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img114.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img115.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img117.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img118.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img120.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img135.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img166.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img167.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img173.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img202.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img213.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img216.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img217.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img219.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img221.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img223.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img224.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img243.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img244.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img246.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img253.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img259.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img261.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img262.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img263.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img272.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img273.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img274.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img301 (2).jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img305 (4).jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img307 (2).jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img308 (2).jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img309.jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img311 (3).jpg",
+         width: 3,
+        height: 2
+    },
+    {
+        src: "https://clayimages.s3.amazonaws.com/img314 (3).jpg",
+         width: 3,
+        height: 2
+    },
+];
