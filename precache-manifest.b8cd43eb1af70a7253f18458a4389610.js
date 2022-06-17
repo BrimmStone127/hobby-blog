7f23000caab6baad3e21d6bde539a73b487ec063
claybrimm <claybrimm@gmail.com>
2022-06-17 15:41:47 -0400

    Updates

---
+++ precache-manifest.b8cd43eb1af70a7253f18458a4389610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b4a9d230f9d511a3143",
+    "url": "/kitchen-buddy/static/css/main.70ea4f5a.chunk.css"
+  },
+  {
+    "revision": "8b4a9d230f9d511a3143",
+    "url": "/kitchen-buddy/static/js/main.8b4a9d23.chunk.js"
+  },
+  {
+    "revision": "7bb8f312306453c5a69a",
+    "url": "/kitchen-buddy/static/css/1.b6c46b81.chunk.css"
+  },
+  {
+    "revision": "7bb8f312306453c5a69a",
+    "url": "/kitchen-buddy/static/js/1.7bb8f312.chunk.js"
+  },
+  {
+    "revision": "5d189e2845edc0233969",
+    "url": "/kitchen-buddy/static/js/runtime~main.5d189e28.js"
+  },
+  {
+    "revision": "b8263f723752f44e31cce1049ed3bd93",
+    "url": "/kitchen-buddy/index.html"
+  }
+];
