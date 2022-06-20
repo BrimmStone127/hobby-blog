6d3788833a30c2572993f7872c1340484c3fabcb
claybrimm <claybrimm@gmail.com>
2022-06-20 09:33:03 -0400

    Updates

---
+++ precache-manifest.fda67e6aeaf07ade474a4bb411f9210f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63787aefd3e8acf1e2c0",
+    "url": "/hobby-blog/static/css/main.70ea4f5a.chunk.css"
+  },
+  {
+    "revision": "63787aefd3e8acf1e2c0",
+    "url": "/hobby-blog/static/js/main.63787aef.chunk.js"
+  },
+  {
+    "revision": "7bb8f312306453c5a69a",
+    "url": "/hobby-blog/static/css/1.b6c46b81.chunk.css"
+  },
+  {
+    "revision": "7bb8f312306453c5a69a",
+    "url": "/hobby-blog/static/js/1.7bb8f312.chunk.js"
+  },
+  {
+    "revision": "e39d9d6fcd535daaf17f",
+    "url": "/hobby-blog/static/js/runtime~main.e39d9d6f.js"
+  },
+  {
+    "revision": "d8b883aecdedbadd8ec0090e917d479b",
+    "url": "/hobby-blog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kitchen-buddy/precache-manifest.150726ec6cc0333287305612eafa8b12.js"
+  "/hobby-blog/precache-manifest.fda67e6aeaf07ade474a4bb411f9210f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kitchen-buddy/index.html", {
+workbox.routing.registerNavigationRoute("/hobby-blog/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
