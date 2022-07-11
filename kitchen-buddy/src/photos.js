1529ba62b1bc76d501b304db35a569916c704073
claybrimm <claybrimm@gmail.com>
2022-07-11 11:41:56 -0400

    Added date_taken field to photos

--- kitchen-buddy/src/photos.js
+++ kitchen-buddy/src/photos.js
@@ -2,431 +2,517 @@ export const photos = [
     {
         src: "https://clayimages.s3.amazonaws.com/img003.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-01"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img029.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img054-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img058.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img059.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-23"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img061 (2).jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-23"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img070.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-23"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img080-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-23"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img097.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img100.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img101.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img103.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img104.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img105.jpg",
          width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img109.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img111.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img112.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img113.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img114.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img115.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img117.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img118.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-27"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img120.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-29"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img133-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-04-29"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img135.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-04-29"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img166.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img167.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img173.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img176-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img177-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img180-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img190-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img202.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img207.jpg",
         width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img213.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img216.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img217.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img218-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img219.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img221.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img226-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img227-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img230-2x2.jpg",
         width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img223.jpg",
          width: 3,
-        height: 2
+        height: 2,
+        date_taken: "2022-05-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img243.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img244.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img246.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img253.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img259.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img261.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img262.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img263.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img272.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img273.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img274.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-05-14"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img301 (2).jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-07"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img305 (4).jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-07"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img307 (2).jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-07"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img308 (2).jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-07"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img309.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-07"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img311 (3).jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-07"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img314 (3).jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-07"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img332.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-10"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img356-3x2.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img357-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img358-3x2.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img359-3x2.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img360-3x2.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img363-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img371-2x2.jpg",
          width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img373-2x2.jpg",
          width: 2,
-        height: 2
+        height: 2,
+        date_taken: "2022-06-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img383.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-19"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img385.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-06-19"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img396-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-06-19"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img401-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-04-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img408-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-04-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img411-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-04-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img413-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-04-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img418-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-04-20"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img488-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-07-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img496.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-07-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img498.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-07-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img503-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-07-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img507.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-07-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img511-2x2.jpg",
-         width: 2,
-        height: 2
+        width: 2,
+        height: 2,
+        date_taken: "2022-07-09"
     },
     {
         src: "https://clayimages.s3.amazonaws.com/img516.jpg",
-         width: 3,
-        height: 2
+        width: 3,
+        height: 2,
+        date_taken: "2022-07-09"
     },
 ];
