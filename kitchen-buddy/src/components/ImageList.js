fa4707f60e86c4031b132cf5c6e94e9782824af6
BrimmStone127 <claybrimm@gmail.com>
2022-12-26 20:32:02 -0600

    Added imageList

--- kitchen-buddy/src/components/ImageList.js
+++ kitchen-buddy/src/components/ImageList.js
@@ -1,1 +1,23 @@
 
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const ImageList = () => {
+  // Use the useSelector hook to get the images from the store
+  const images = useSelector(state => state.images);
+
+  // Render the images
+  return (
+    <ul>
+      {images.map(image => (
+        <li key={image.url}>
+          <img src={image.url} alt={image.title} />
+          <p>{image.title}</p>
+          <p>{image.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ImageList;
